dd681d3030a0383e882567139b703d094f73d034
chukwunonsosylva17 <okoyechukwunonso17@gmail.com>
2023-04-14 12:47:49 +0100

    added a new function

--- multiplication_table.js
+++ multiplication_table.js
@@ -3,4 +3,8 @@ let num = 1;
 let tableOf = 12;
 for (let num = 1; num <= 15; num++ ) { 
     console.log(`${tableOf} X ${num} = ${tableOf*num}`);
-};
+};    
+
+const newFunct = () => {
+    console.log("my functions")
+}
